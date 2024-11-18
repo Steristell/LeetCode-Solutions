402dce6a5b8a1537401eaece67e3348dd82085d7
Steristell <ian@davidsoncrew.com>
2024-11-18 11:28:28 -0500

    Python Solution
            Submission Statistics:
            Question #: 1755
            Language: Python
            Runtime: 19 ms
            Runtime percentile: 7.43
            Memory: 11.42 MB
            Memory percentile: 94.42

---
+++ 1755. Defuse the Bomb/Solution.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def decrypt(self, code, k):
+        
+        def next(c, l):
+            if l < len(c) - 1:
+                l = l + 1
+            else:
+                l = 0
+            return l
+        def prev(c, l):
+            if l == 0:
+                l = len(c) - 1
+            else:
+                l = l - 1
+            return l
+        if k == 0:
+            for i in range(len(code)):
+                code[i] = 0
+            return code
+        result = []
+        if k > 0:
+            for i in range(len(code)):
+                curr = i
+                val = 0
+                for j in range(k):
+                    curr = next(code, curr)
+                    val += code[curr]
+                result.append(val)
+            return result
+        if k < 0:
+            for i in range(len(code)):
+                curr = i
+                val = 0
+                for j in range(abs(k)):
+                    curr = prev(code, curr)
+                    val += code[curr]
+                result.append(val)
+            return result
+
+        """
+        :type code: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
