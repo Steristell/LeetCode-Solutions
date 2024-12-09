726d2dc6c7532aa2cec011620193db2296aca973
Steristell <ian@davidsoncrew.com>
2024-12-09 12:56:38 -0500

    Python Solution
            Submission Statistics:
            Question #: 3427
            Language: Python
            Runtime: 96 ms
            Runtime percentile: 50.00
            Memory: 42.432 MB
            Memory percentile: 66.67

---
+++ 3427. Special Array II/Solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isArraySpecial(self, nums, queries):
+        n = len(nums)
+        store, ans = [], []
+
+        for i in range(1, n):
+            if (nums[i] % 2) == (nums[i - 1] % 2):
+                store.append(i)
+
+        for left, right in queries:
+            idx = bisect_right(store, left)
+
+            if idx < len(store) and  store[idx] <= right: ans.append(False)
+            else: ans.append(True)
+        
+        return ans
+            
